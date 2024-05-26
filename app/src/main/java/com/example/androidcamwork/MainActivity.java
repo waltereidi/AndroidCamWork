3d387b945a34b350b7aec951b661781d28c4d065
walter-eidi <walter-eidi@hotmail.com>
2024-05-26 20:26:41 -0300

    feito botão que chama camera e colocado imagem em view

--- app/src/main/java/com/example/androidcamwork/MainActivity.java
+++ app/src/main/java/com/example/androidcamwork/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androidcamwork;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,13 +14,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -27,6 +27,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +46,21 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            if(takePictureIntent.resolveActivity(this.getPackageManager()) != null)
-            {
-                File photoFile = null ;
-                photoFile = this.getPackageManager() ;
-            }
 
         } catch (ActivityNotFoundException e) {
             // display error state to the user
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView view = this.findViewById(R.id.imageView);
+            view.setImageBitmap(imageBitmap);
+
         }
     }
 
